b324838730b7c72af22d6042468bdc6c7cd9a69f
ezoza <ezozakhudayberdieva@mail.ru>
2023-02-28 01:03:39 +0300

    feat: added failess

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
 // You should implement your task here.
+module.exports = function towelSort(matrix) {
+  let arr = []
+  if (typeof matrix != 'object' ) {return arr}
+  if (matrix.length === 0 ) {return arr}
 
-module.exports = function towelSort (matrix) {
-  return [];
+  for (i = 0; i < matrix.length; i++) {
+     if (i % 2 != 0) {
+        const reverseArray = matrix[i].reverse()  
+        for (j=0; j < reverseArray.length; j++ ) {
+           arr.push(reverseArray[j])
+        }
+     } else {
+        for (j=0; j < matrix[i].length; j++ ) {
+           arr.push(matrix[i][j])
+        }
+     }
+
+  }
+  return array
 }
+
